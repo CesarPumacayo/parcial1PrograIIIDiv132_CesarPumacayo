1682912aaa09425e9c6ca569d780b7f5ea648fc7
César Pumacayo <cesarbjh@gmail.com>
2025-10-08 09:24:17 -0300

    cambios repositorios

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const frutas = [
+    {
+        id: 1,
+        nombre: "anana",
+        precio: 10,
+        ruta_img :  "../img/anana.jpg"
+    },
+    {id: 2,nombre: "arandano",precio: 15,ruta_img :  "../img/arandano.jpg"},
+    {id: 3,nombre: "banana",precio: 20,ruta_img :  "../img/banana.jpg" },
+    {id: 4,nombre: "frambuesa",precio: 40,ruta_img :  "../img/Frambuesa.png"},
+    {id: 5,nombre: "frutilla",precio: 80,ruta_img :  "../img/frutilla.jpg"},
+    {id: 6,nombre: "kiwi",precio: 100,ruta_img :  "../img/kiwi.jpg"},
+    {id: 7, nombre: "mandarina", precio: 34, ruta_img : "../img/mandarina.jpg"},
+    {id: 8, nombre: "manzana", precio: 50, ruta_img: "../img/manzana.jpg"},
+    {id: 9, nombre: "naranja", precio: 12, ruta_img: "../img/naranja.jpg"},
+    {id: 10, nombre: "pera", precio: 32, ruta_img: "../img/pera.jpg"},
+    {id: 11, nombre: "pomelo amarillo", precio: 25, ruta_img: "../img/pomelo-amarillo.jpg"},
+    {id: 12, nombre: "pomelo rojo", precio: 25, ruta_img: "../img/pomelo-rojo.jpg"},
+    {id: 13, nombre: "sandia", precio: 90, ruta_img: "../img/sandia.jpg"},
+]; 
+
